ec49c2a0819ba815d264b00849f8bf0166ff5549
riznykalex <riznyk.alex.mail@gmail.com>
2022-11-13 20:29:50 +0200

    new file:   weather/weather-app/.gitignore 	new file:   weather/weather-app/README.md 	new file:   weather/weather-app/package-lock.json 	new file:   weather/weather-app/package.json 	new file:   weather/weather-app/public/favicon.ico 	new file:   weather/weather-app/public/index.html 	new file:   weather/weather-app/public/logo192.png 	new file:   weather/weather-app/public/logo512.png 	new file:   weather/weather-app/public/manifest.json 	new file:   weather/weather-app/public/robots.txt 	new file:   weather/weather-app/src/App.css 	new file:   weather/weather-app/src/App.js 	new file:   weather/weather-app/src/App.test.js 	new file:   weather/weather-app/src/api.js 	new file:   weather/weather-app/src/components/search/search.js 	new file:   weather/weather-app/src/index.css 	new file:   weather/weather-app/src/index.js 	new file:   weather/weather-app/src/logo.svg 	new file:   weather/weather-app/src/reportWebVitals.js 	new file:   weather/weather-app/src/setupTests.js

---
+++ weather/weather-app/src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd47cd77d1dmsh35a5405428ff7a1p1bcdb3jsna6bc2d1a83ae',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
---
+++ weather/weather-app/src/components/search/search.js
@@ -0,0 +1,47 @@
+import {
+    useState
+} from "react";
+import {
+    AsyncPaginate
+} from "react-select-async-paginate"
+import {
+    GEO_API_URL,
+    geoApiOptions
+} from "../../api"
+const Search = (onSearchChange) => {
+
+    const [search, setSearch] = useState(null);
+
+    const loadOptions = (inputValues) => {
+        return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValues}`,     geoApiOptions
+        )
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(err => console.error(err));
+
+    }
+    const handleOnChange = (searchData) => {
+        setSearch(searchData)
+        onSearchChange(searchData)
+    }
+
+    return ( <
+        AsyncPaginate placeholder = "Search for city"
+        debounceTimeout = {
+            1000
+        }
+        value = {
+            search
+        }
+        onChange = {
+            handleOnChange
+        }
+        loadOptions = {
+            loadOptions
+        }
+        />   
+
+    )
+}
+
+export default Search;
